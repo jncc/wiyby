5e8b4bd45389f63fde3c8daf5e7d4c39928bd260
felix mason <felix.mason@jncc.gov.uk>
2016-02-25 11:44:16 +0000

    Slightly more complex hello world as described in case

---
+++ backend/backend/src/main/java/uk/gov/defra/jncc/wff/resources/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.gov.defra.jncc.wff.resources;
+
+/**
+ *
+ * @author felix
+ */
+public class Hello {
+    public String hello;
+    
+    public Hello(String hello)
+    {
+        this.hello = hello;
+    }
+}
