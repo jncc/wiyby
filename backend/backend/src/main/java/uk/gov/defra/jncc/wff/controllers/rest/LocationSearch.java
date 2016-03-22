d38a7738e8aec93535beb72adcb94360ac8bbd3a
felix mason <felix.mason@jncc.gov.uk>
2016-03-22 09:06:04 +0000

    commit of search changes back to simgle page

--- backend/backend/src/main/java/uk/gov/defra/jncc/wff/controllers/rest/LocationSearch.java
+++ backend/backend/src/main/java/uk/gov/defra/jncc/wff/controllers/rest/LocationSearch.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.defra.jncc.wff.resources.Location;
-import uk.gov.defra.jncc.wff.resources.LocationResult;
 import uk.gov.defra.jncc.wff.resources.statics.OSKeys;
 import uk.gov.defra.jncc.wff.services.ReprojectionService;
 import uk.gov.defra.jncc.wff.services.RestClientService;
@@ -50,26 +49,30 @@ public class LocationSearch {
     @ApiOperation(value = "Location search", 
         response = String.class, 
         responseContainer = "Page")
-    public ResponseEntity<LocationResult> getLocationByName(
+    public ResponseEntity<uk.gov.defra.jncc.wff.resources.LocationResult> getLocationByName(
             @ApiParam(value = "The location query")
             @RequestParam(name = "query")  String query) throws Exception {   
         String apiUrl = "https://api.ordnancesurvey.co.uk/opennames/v1/find?query=";
         String queryUrl = apiUrl + URLEncoder.encode(query,"UTF-8") + "&key=" + OSKeys.OS_NAMES_KEY;
         String jsonResponse = "";
         
+        uk.gov.defra.jncc.wff.resources.LocationResult result = new uk.gov.defra.jncc.wff.resources.LocationResult();
+        result.setQuery(query);
+        
+        HttpStatus status = HttpStatus.OK;
+        
         try 
         {
             jsonResponse = client.Get(queryUrl);
+            ArrayList<Location> locations = osLocationParser.GetMachingLocations(jsonResponse, query);
+            result.setLocations(locations);
         }
         catch (ClientProtocolException e)
         {
-            return new ResponseEntity("Invalid search term", HttpStatus.BAD_REQUEST);
+            result.addError("bad_request", "Invalid location query");
+            status = HttpStatus.BAD_REQUEST;
         }
 
-        ArrayList<Location> locations = osLocationParser.GetMachingLocations(jsonResponse, query);
-        
-        LocationResult result = new LocationResult(query, locations);
-       
         return new ResponseEntity(result, HttpStatus.OK);
     }
     
@@ -78,7 +81,7 @@ public class LocationSearch {
     @ApiOperation(value = "Location search", 
         response = String.class, 
         responseContainer = "Page")
-    public HttpEntity<LocationResult> getLocationByBbox(
+    public HttpEntity<uk.gov.defra.jncc.wff.resources.LocationResult> getLocationByBbox(
             @ApiParam(value = "The location query")
             @RequestParam(name = "bbox")  String bbox,
             @ApiParam(value = "The SRS of the bounding box")
---
+++ backend/backend/src/main/java/uk/gov/defra/jncc/wff/controllers/web/LocationController.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.gov.defra.jncc.wff.controllers.web;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import uk.gov.defra.jncc.wff.controllers.rest.LocationSearch;
+import uk.gov.defra.jncc.wff.resources.Base;
+import uk.gov.defra.jncc.wff.resources.Location;
+import uk.gov.defra.jncc.wff.resources.LocationResult;
+
+/**
+ *
+ * @author felix
+ */
+@Controller
+public class LocationController {
+
+    @Autowired
+    LocationSearch locationSearch;
+
+    @RequestMapping(value = "/web/location", method = RequestMethod.GET)
+    public String GetLocationsForm(Model model) {
+        model.addAttribute("LocationResult", new LocationResult());
+
+        return "find";
+    }
+
+    @RequestMapping(value = "/web/location", method = RequestMethod.POST)
+    public String GetLocationsSubmit(
+            @ModelAttribute LocationResult search,
+            Model model) throws Exception {
+        
+        //humm try catch but don't want to reveal error to end user?
+        ResponseEntity<LocationResult> httpSearchResult = locationSearch.getLocationByName(search.getQuery());
+
+        if (httpSearchResult.getStatusCode() == HttpStatus.OK) {
+            LocationResult searchResult = httpSearchResult.getBody();
+
+            if (searchResult.getLocations().size() == 1) {
+                //redirct to map page with sigle result
+            }
+
+            List<Location> topLocations = searchResult.getLocations().stream()
+                    .limit(5)
+                    .collect(Collectors.toList());
+
+            
+            LocationResult result = new LocationResult();
+            
+            result.setQuery(searchResult.getQuery());
+            result.setLocations(topLocations);
+            
+            model.addAttribute("LocationResult", result);
+
+            return "locations";
+
+        } else {
+            model.addAttribute("error", httpSearchResult.getBody().getBase().get(Base.ERROR));
+            return "error";
+        }
+    }
+}
