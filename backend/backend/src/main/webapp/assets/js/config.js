e50c68f13e2a590c149cffd95387881f3defb8f9
Matt Debont <matt.debont@jncc.gov.uk>
2016-03-23 09:33:35 +0000

    Moved out some config options into their own separate script

---
+++ backend/backend/src/main/webapp/assets/js/config.js
@@ -0,0 +1,6 @@
+/**
+ * Global configuration options
+ */
+
+var os_mapping_url = "https://api2.ordnancesurvey.co.uk/mapping_api/v1/service/zxy/EPSG%3A3857/Outdoor%203857/{z}/{x}/{y}.png?key={{{os_map_key}}}";
+var local_wms_service_url = "http://52.31.39.116/geoserver/ows?service=wms&version=1.3.0";
